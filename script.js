389b1122a06c2246816f49b46a3a6146cd1e64bf
Dan <dandemereckis@yahoo.com>
2026-02-13 19:33:49 -0600

    Adde more JS functionality
    Added prompt to allow users to change the size of
    the grid.
    Also added hover effect.

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const containerEl = document.querySelector(".container");
+const btnEl = document.querySelector(".btn");
 
-let size = Number(prompt("How big would you like?"));
+let size;
 
-for (let i = 1; i <= size; i++) {
-  const boxEl = document.createElement("div");
-  boxEl.style.flexBasis = 100 / Math.sqrt(size) + "%";
-  boxEl.classList.add("box");
-  containerEl.appendChild(boxEl);
+function getSize() {
+  size =
+    Number(prompt("How many numbers per side of the grid would you like?")) **
+    2;
+  if (size > 10000) {
+    alert("Cannot pick a number greater than 100");
+    size =
+      Number(prompt("How many numbers per side of the grid would you like?")) **
+      2;
+  } else {
+    return size;
+  }
 }
+
+function createGrid() {
+  getSize();
+  for (let i = 1; i <= size; i++) {
+    const boxEl = document.createElement("div");
+    boxEl.style.flexBasis = 100 / Math.sqrt(size) + "%";
+    boxEl.classList.add("box");
+    containerEl.appendChild(boxEl);
+  }
+}
+
+btnEl.addEventListener("click", () => {
+  containerEl.innerHTML = "";
+  createGrid();
+});
+
+createGrid();
